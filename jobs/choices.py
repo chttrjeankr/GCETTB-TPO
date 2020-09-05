817150b22c65724efe36dc3b3caf363deb6fafc6
Ankur Chattopadhyay <chttrjeankr@gmail.com>
2020-09-05 11:47:06 +0530

    fix: choices should be list, to avoid unnecessary migrations

--- jobs/choices.py
+++ jobs/choices.py
@@ -1,13 +1,13 @@
-JOB_TYPES = {
+JOB_TYPES = [
     ("FT", "Full Time Job"),
     ("IT", "Internship"),
     ("EC", "Exam/Competition"),
-}
+]
 
-PLATFORM_CHOICES = {
+PLATFORM_CHOICES = [
     ("LNKD", "LinkedIn"),
     ("YOUT", "YouTube"),
     ("INTS", "Internshala"),
     ("CMPY", "Company Page"),
     ("OTHR", "Others"),
-}
+]
